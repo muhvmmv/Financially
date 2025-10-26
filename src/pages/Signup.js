82a070e9e3b6a05f7fbc58dc987b8530399ea370
muhvmmv <mnasir44744@gmail.com>
2025-10-26 20:13:52 +0100

    third commit

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { PieChart, Eye, EyeOff, Mail, Lock, User } from "lucide-react";
 import axios from 'axios';
 
